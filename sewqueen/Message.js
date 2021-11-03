1877c3baaa286aa54d29a7a07d4cc01f88ffaaea
toddy77 <93471943+toddy77@users.noreply.github.com>
2021-11-04 01:15:21 +0530

    Update Message.js

--- sewqueen/Message.js
+++ sewqueen/Message.js
@@ -1,9 +1,9 @@
-/* Coded By Ravindu Manoj.
+/* Coded By AASHIF.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Whats bot - Ravindu Manoj
+Whats bot - Toddy
 */
 
 const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
